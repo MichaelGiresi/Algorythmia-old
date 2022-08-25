eed0a0c4598f92559cd5bf84e396cd5990b7a78b
MichaelGiresi <mikegiresidev@gmail.com>
2022-08-25 17:18:57 -0400

    Removed Nav.jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Hero from "./home/components/Hero";
-import Nav from "./home/components/Nav";
 import Upcomming from "./home/components/Upcomming";
 import Video from "./home/components/Video";
 import nav from '../src/home/styles/nav/nav.css'
