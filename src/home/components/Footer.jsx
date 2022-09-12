7790b9f62faeb51caa11d2d20b25a87d5ec4c429
MichaelGiresi <mikegiresidev@gmail.com>
2022-09-12 11:04:18 -0400

    fixed shop all link in homepage footer

--- src/home/components/Footer.jsx
+++ src/home/components/Footer.jsx
@@ -1,10 +1,11 @@
 import footer from '../styles/footer/footer.css'
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 const Footer = () => {
 
   return (
 
     <div className='footer'>
-      <a href='/products' className='footer-shop-all'>Shop All</a>
+      <Link to='/products' onClick={() => window.scrollTo(0, 0)} href="#top" className='footer-shop-all'>Shop All</Link>
       <div className='footer-oval'></div>
     </div>
     
